0f51bb351d8ba6a55d55260cdd660a2a8ca839d8
Jorin Perquy <jorin.perquy@xandr.com>
2023-06-29 12:54:20 +0200

    add day 8 2017

---
+++ src/test/java/aoc2017/Day8Test.java
@@ -0,0 +1,40 @@
+package aoc2017;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class Day8Test {
+
+    @Test
+    void test_first_star() {
+        Day8 day = new Day8(List.of("b inc 5 if a > 1",
+                "a inc 1 if b < 5",
+                "c dec -10 if a >= 1",
+                "c inc -20 if c == 10"));
+
+        assertEquals(1, day.firstStar());
+    }
+
+    @Test
+    void test_second_star() {
+        Day8 day = new Day8(List.of("b inc 5 if a > 1",
+                "a inc 1 if b < 5",
+                "c dec -10 if a >= 1",
+                "c inc -20 if c == 10"));
+
+        assertEquals(10, day.secondStar());
+    }
+
+    @Test
+    void test_real_inputs() {
+        Day8 day = new Day8();
+
+        assertEquals(4567, day.firstStar());
+        assertEquals(5636, day.secondStar());
+    }
+
+}
