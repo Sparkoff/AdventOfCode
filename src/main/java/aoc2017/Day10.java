48a26463a0ed558697a2e6de93da2d542f19a773
Jorin Perquy <jorin.perquy@gmail.com>
2023-09-16 00:17:39 +0200

    add day 14 2017

--- src/main/java/aoc2017/Day10.java
+++ src/main/java/aoc2017/Day10.java
@@ -35,36 +35,19 @@ public class Day10 extends DayBase<String, String> {
     public String firstStar() {
         String lengths = this.getInput(PuzzleInput::asString);
 
-        Step init = new Step(initHash());
-        List<Integer> hash = computeHash(toListOfInt(lengths), init).hash();
+        Step init = new Step(initHash(hashSize));
+        List<Integer> hash = computeHash(toListOfInt(lengths), init, hashSize).hash();
         return String.valueOf(hash.get(0) * hash.get(1));
     }
 
     @Override
     public String secondStar() {
         String word = this.getInput(PuzzleInput::asString);
-        List<Integer> asciiWord = toAsciiList(word);
-
-        Step step = new Step(initHash());
-        for (int i = 0; i < 64; i++) {
-            step = computeHash(asciiWord, step);
-        }
-
-        List<Integer> hash = step.hash();
-        return IntStream.range(0, hashSize)
-                .boxed()
-                .collect(Collectors.groupingBy(i -> i / 16))
-                .values().stream()
-                .map(idxs -> idxs.stream()
-                        .map(hash::get)
-                        .collect(Collectors.toList()))
-                .map(this::toDense)
-                .map(this::toHexa)
-                .collect(Collectors.joining(""));
+        return generateKnotHash(word, hashSize);
 
     }
 
-    private List<Integer> initHash() {
+    private static List<Integer> initHash(int hashSize) {
         return IntStream.range(0, hashSize)
                 .boxed()
                 .collect(Collectors.toList());
@@ -74,7 +57,7 @@ public class Day10 extends DayBase<String, String> {
                 .map(Integer::parseInt)
                 .toList();
     }
-    private List<Integer> toAsciiList(String input) {
+    private static List<Integer> toAsciiList(String input) {
         List<Integer> suffix = List.of(17, 31, 73, 47, 23);
 
         if (input.isEmpty()) return suffix;
@@ -86,17 +69,17 @@ public class Day10 extends DayBase<String, String> {
         return ascii;
     }
 
-    private Step computeHash(List<Integer> lengths, Step step) {
+    private static Step computeHash(List<Integer> lengths, Step step, int hashSize) {
         for (int l : lengths) {
             List<Integer> hash = step.hash();
             if (l > 1) {
                 int lengthMid = l / 2;
 
                 for (int li = 0; li < lengthMid; li++) {
-                    int firstIdx = circularIndex(step.currentIdx() + li);
+                    int firstIdx = circularIndex(step.currentIdx() + li, hashSize);
                     int firstElement = hash.get(firstIdx);
 
-                    int secondIdx = circularIndex(step.currentIdx() + l - 1 - li);
+                    int secondIdx = circularIndex(step.currentIdx() + l - 1 - li, hashSize);
                     int secondElement = hash.get(secondIdx);
 
                     hash.set(firstIdx, secondElement);
@@ -105,7 +88,7 @@ public class Day10 extends DayBase<String, String> {
             }
 
             step = new Step(
-                    circularIndex(step.currentIdx() + l + step.skip()),
+                    circularIndex(step.currentIdx() + l + step.skip(), hashSize),
                     step.skip() + 1,
                     hash
             );
@@ -114,13 +97,37 @@ public class Day10 extends DayBase<String, String> {
         return step;
     }
 
-    private int circularIndex(int idx) {
+    private static int circularIndex(int idx, int hashSize) {
         return idx % hashSize;
     }
-    private int toDense(List<Integer> hash) {
+    private static int toDense(List<Integer> hash) {
         return hash.stream().reduce(0, (a, c) -> a ^ c);
     }
-    private String toHexa(int denseHash) {
+    private static String toHexa(int denseHash) {
         return String.format("%02x", denseHash);
+    }
+
+    private static String generateKnotHash(String word, int hashSize) {
+        List<Integer> asciiWord = toAsciiList(word);
+
+        Step step = new Step(initHash(hashSize));
+        for (int i = 0; i < 64; i++) {
+            step = computeHash(asciiWord, step, hashSize);
+        }
+
+        List<Integer> hash = step.hash();
+        return IntStream.range(0, hashSize)
+                .boxed()
+                .collect(Collectors.groupingBy(i -> i / 16))
+                .values().stream()
+                .map(idxs -> idxs.stream()
+                        .map(hash::get)
+                        .collect(Collectors.toList()))
+                .map(Day10::toDense)
+                .map(Day10::toHexa)
+                .collect(Collectors.joining(""));
+    }
+    public static String generateKnotHash(String word) {
+        return generateKnotHash(word, 256);
     }
 }
