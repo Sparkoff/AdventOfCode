c055133d41572f49868551c58980333ee7a8f6cb
Jorin Perquy <jorin.perquy@gmail.com>
2023-09-22 23:11:22 +0200

    add day 24 2017

---
+++ src/main/java/aoc2017/Day24.java
@@ -0,0 +1,98 @@
+package aoc2017;
+
+import common.DayBase;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class Day24 extends DayBase<List<Day24.Component>, Integer> {
+
+    public Day24() {
+        super();
+    }
+
+    public Day24(List<String> input) {
+        super(input);
+    }
+
+    record Component(int left, int right) {
+        public int strength() {
+            return left + right;
+        }
+
+        public boolean hasType(int type) {
+            return left == type || right == type;
+        }
+
+        public int next(int type) {
+            return left == type ? right : left;
+        }
+    }
+
+    @Override
+    public Integer firstStar() {
+        List<Component> components = this.getInput(Day24::parseComponents);
+
+        return makeBridges(components, 0).stream()
+                .mapToInt(b -> b.stream()
+                        .mapToInt(Component::strength)
+                        .sum())
+                .max()
+                .orElseThrow();
+    }
+
+    @Override
+    public Integer secondStar() {
+        List<Component> components = this.getInput(Day24::parseComponents);
+
+        return makeBridges(components, 0).stream()
+                .collect(Collectors.groupingBy(List::size))
+
+                .entrySet().stream()
+                .max(Comparator.comparingInt(Map.Entry::getKey))
+                .orElseThrow()
+
+                .getValue().stream()
+                .mapToInt(b -> b.stream()
+                        .mapToInt(Component::strength)
+                        .sum())
+                .max()
+                .orElseThrow();
+    }
+
+    private static List<List<Component>> makeBridges(List<Component> availableComponents,  int nextType) {
+        List<Component> nextComponents = availableComponents.stream()
+                .filter(c -> c.hasType(nextType))
+                .toList();
+
+        List<List<Component>> bridges = new ArrayList<>();
+        for (Component component : nextComponents) {
+            bridges.add(List.of(component));
+            List<List<Component>> nextBridges = makeBridges(
+                    availableComponents.stream()
+                            .filter(c -> c != component)
+                            .toList(),
+                    component.next(nextType)
+            );
+            if (!nextBridges.isEmpty()) {
+                bridges.addAll(nextBridges.stream()
+                        .map(b -> Stream.concat(Stream.of(component), b.stream()).toList())
+                        .toList());
+            }
+        }
+
+        return bridges;
+    }
+
+    private static List<Component> parseComponents(List<String> input) {
+        return input.stream()
+                .map(l -> l.split("/"))
+                .map(l -> new Component(Integer.parseInt(l[0]), Integer.parseInt(l[1])))
+                .toList();
+    }
+}
---
+++ src/test/java/aoc2017/Day24Test.java
@@ -0,0 +1,48 @@
+package aoc2017;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class Day24Test {
+
+    @Test
+    void test_first_star() {
+        Day24 day = new Day24(List.of("0/2",
+                "2/2",
+                "2/3",
+                "3/4",
+                "3/5",
+                "0/1",
+                "10/1",
+                "9/10"));
+
+        assertEquals(31, day.firstStar());
+    }
+
+    @Test
+    void test_second_star() {
+        Day24 day = new Day24(List.of("0/2",
+                "2/2",
+                "2/3",
+                "3/4",
+                "3/5",
+                "0/1",
+                "10/1",
+                "9/10"));
+
+        assertEquals(19, day.secondStar());
+    }
+
+    @Test
+    void test_real_inputs() {
+        Day24 day = new Day24();
+
+        assertEquals(1940, day.firstStar());
+        assertEquals(1928, day.secondStar());
+    }
+
+}
