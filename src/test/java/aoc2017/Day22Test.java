06ed6eb30a15dfa87e3b3c5da966a2a34b5d0f1c
Jorin Perquy <jorin.perquy@gmail.com>
2023-09-22 16:00:04 +0200

    add day 22 2017

---
+++ src/test/java/aoc2017/Day22Test.java
@@ -0,0 +1,38 @@
+package aoc2017;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class Day22Test {
+
+    @Test
+    void test_first_star() {
+        Day22 day = new Day22(List.of("..#",
+                "#..",
+                "..."));
+
+        assertEquals(5587, day.firstStar());
+    }
+
+    @Test
+    void test_second_star() {
+        Day22 day = new Day22(List.of("..#",
+                "#..",
+                "..."));
+
+        assertEquals(2511944, day.secondStar());
+    }
+
+    @Test
+    void test_real_inputs() {
+        Day22 day = new Day22();
+
+        assertEquals(5369, day.firstStar());
+        assertEquals(2510774, day.secondStar());
+    }
+
+}
