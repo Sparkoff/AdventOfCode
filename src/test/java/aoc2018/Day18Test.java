e4ea4e4afb7771187b99c9588121331c277214e2
Jorin Perquy <jorin.perquy@gmail.com>
2023-12-22 19:49:16 +0100

    add day 18 2018

---
+++ src/test/java/aoc2018/Day18Test.java
@@ -0,0 +1,38 @@
+package aoc2018;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@DisplayName("2018 Day18")
+class Day18Test {
+
+    @Test
+    void test_first_star() {
+        Day18 day = new Day18(List.of(".#.#...|#.",
+                ".....#|##|",
+                ".|..|...#.",
+                "..|#.....#",
+                "#.#|||#|#|",
+                "...#.||...",
+                ".|....|...",
+                "||...#|.#|",
+                "|.||||..|.",
+                "...#.|..|."));
+
+        assertEquals(1147, day.firstStar());
+    }
+
+    @Test
+    void test_real_inputs() {
+        Day18 day = new Day18();
+
+        assertEquals(614812, day.firstStar());
+        assertEquals(212176, day.secondStar());
+    }
+
+}
